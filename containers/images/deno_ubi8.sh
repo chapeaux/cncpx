68866c0eed2e7bb55b15aaf4e3c7fea70e0ab2d3
Luke Dary <lukedary@gmail.com>
2021-04-07 10:49:57 -0400

    Latest updates

--- containers/images/deno_ubi8.sh
+++ containers/images/deno_ubi8.sh
@@ -7,11 +7,12 @@ buildah config --workingdir /projects $ctr
 buildah config --env DENO_INSTALL=/usr/local $ctr
 buildah config --env DENO_DIR=/projects/.deno $ctr
 buildah config --env PATH=$PATH:/usr/local/bin $ctr
+#buildah run --isolation rootless $ctr /bin/sh -c "curl -fsSL https://deno.land/x/install/install.sh | bash;"
 buildah run --isolation rootless $ctr /bin/sh -c "microdnf update; \
 microdnf -y install unzip; \
 curl -fsSL https://deno.land/x/install/install.sh | sh; \
 microdnf clean all;"
-buildah config --entrypoint "tail -f /dev/null" $ctr
+#buildah config --entrypoint "tail -f /dev/null" $ctr
 buildah config --author "Luke Dary" --created-by "ldary" --label name="${ctrimg}" $ctr
 buildah unmount $ctr
 buildah commit --squash $ctr $ctrimg
