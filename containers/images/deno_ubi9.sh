dd3dc95ae2d94a233bc2e20c3b8d01d7a52488a2
Luke Dary <lukedary@gmail.com>
2021-11-03 21:32:24 -0400

    Adding ubi9 script

--- containers/images/deno_ubi9.sh
+++ containers/images/deno_ubi9.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ctrimg=deno
 denover=1.15.3
-ctr=$(buildah from registry.access.redhat.com/ubi8/ubi-minimal)
+ctr=$(buildah from registry.access.redhat.com/ubi9-beta/ubi-minimal)
 mountpoint=$(buildah mount $ctr)
 mkdir -p $mountpoint/projects/.deno
 buildah config --workingdir /projects $ctr
@@ -14,11 +14,11 @@ curl -fsSL https://deno.land/x/install/install.sh | sh -s v${denover}; \
 microdnf clean all;"
 buildah config --author "Luke Dary" --created-by "kamiquasi" --label name="${ctrimg}" $ctr
 buildah commit --squash $ctr $ctrimg
-buildah push $ctrimg docker://quay.io/chapeaux/deno:$denover
-buildah push $ctrimg docker://quay.io/chapeaux/deno:latest
+buildah push $ctrimg docker://quay.io/chapeaux/deno:$denover-ubi9
+buildah push $ctrimg docker://quay.io/chapeaux/deno:latest-ubi9
 buildah config --entrypoint "sleep infinity" $ctr
 buildah unmount $ctr
 buildah commit --squash $ctr $ctrimg
-buildah push $ctrimg docker://quay.io/chapeaux/deno-che:$denover
-buildah push $ctrimg docker://quay.io/chapeaux/deno-che:latest
+buildah push $ctrimg docker://quay.io/chapeaux/deno-che:$denover-ubi9
+buildah push $ctrimg docker://quay.io/chapeaux/deno-che:latest-ubi9
 buildah rm $ctr
