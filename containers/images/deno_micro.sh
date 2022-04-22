0e273c216ec21e14c33e4ebfa645a41c7214a37b
Luke Dary <lukedary@gmail.com>
2022-04-22 08:25:54 -0400

    Add deno_micro image build script

--- containers/images/deno_micro.sh
+++ containers/images/deno_micro.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-ctrimg=deno
-denover=1.18.3
+ctrimg=deno-micro
+denover=1.21.0
 ctr=$(buildah from registry.access.redhat.com/ubi8/ubi-micro)
 mountpoint=$(buildah mount $ctr)
 dnf install --installroot $mountpoint \
@@ -8,13 +8,8 @@ dnf install --installroot $mountpoint \
 --setopt install_weak_deps=false \
 --nodocs -y unzip libgcc
 curl -fsSL https://deno.land/x/install/install.sh | DENO_INSTALL=$mountpoint/usr/local sh
-buildah config --author "Luke Dary" --created-by "kamiquasi" --label name="${ctrimg}" $ctr
 buildah commit --squash $ctr $ctrimg
+buildah config --author "Luke Dary" --created-by "kamiquasi" --label name="${ctrimg}" $ctr
 buildah push $ctrimg docker://quay.io/chapeaux/deno:$denover
 buildah push $ctrimg docker://quay.io/chapeaux/deno:latest
-buildah config --entrypoint "sleep infinity" $ctr
 buildah unmount $ctr
-buildah commit --squash $ctr $ctrimg
-buildah push $ctrimg docker://quay.io/chapeaux/deno-che:$denover
-buildah push $ctrimg docker://quay.io/chapeaux/deno-che:latest
-buildah rm $ctr
